4c09cb2733a8c20fa172693cd3bf75e9c1a1512d
Yuri Albuquerque <yuridenommus@gmail.com>
2022-07-28 10:11:11 -0300

    Add nix sandboxes and derivations

---
+++ flake.nix
@@ -0,0 +1,94 @@
+{
+  inputs = {
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    cargo2nix.inputs.rust-overlay.follows = "rust-overlay";
+  };
+
+  outputs = { cargo2nix, flake-utils, nixpkgs, ... }:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    many-rs-rev = "20541988a8722a9bd10e2bcf3cb84dc17e1775e4";
+    many-framework-rev = "a8804085bcc28b75ac8333622f217e0da13bc577";
+    rust-overrides = [
+      (pkgs.rustBuilder.rustLib.makeOverride {
+        name = "cryptoki-sys";
+        overrideAttrs = drv: {
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+          nativeBuildInputs = [
+            pkgs.llvmPackages.libcxxClang
+          ];
+        };
+      })
+      (pkgs.rustBuilder.rustLib.makeOverride {
+        name = "librocksdb-sys";
+        overrideAttrs = drv: {
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+          nativeBuildInputs = [
+            pkgs.llvmPackages.libcxxClang
+          ];
+        };
+      })
+      (pkgs.rustBuilder.rustLib.makeOverride {
+        name = "many-ledger";
+        overrideAttrs = drv: {
+          prePatch = ''
+            substituteInPlace build.rs --replace 'vergen(config).expect("Vergen could not run.")' ""
+          '';
+          VERGEN_GIT_SHA = many-framework-rev;
+        };
+      })
+    ];
+
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [
+        cargo2nix.overlays.default
+
+        (final: prev: {
+          many-rs-pkgs = pkgs.rustBuilder.makePackageSet {
+            rustVersion = "1.62.0";
+            packageFun = import ./many-rs/Cargo.nix;
+            workspaceSrc = final.fetchFromGitHub {
+              owner = "liftedinit";
+              repo = "many-rs";
+              rev = many-rs-rev;
+              sha256 = "sha256-f6ULbOt3mb5IBeaXWDqttPWBWH1/0r3URFJ+HQVtcNs=";
+            };
+            packageOverrides = pkgs: final.rustBuilder.overrides.all ++ rust-overrides;
+          };
+
+          many-framework-pkgs = pkgs.rustBuilder.makePackageSet {
+            rustVersion = "2022-07-27";
+            rustChannel = "nightly";
+            packageFun = import ./many-framework/Cargo.nix;
+            workspaceSrc = final.fetchFromGitHub {
+              owner = "liftedinit";
+              repo = "many-framework";
+              rev = many-framework-rev;
+              sha256 = "sha256-RY5cqa35J+Cmps8LG4Evvq6cH0oJkrOLhoWwdMJymMk=";
+            };
+            packageOverrides = pkgs: final.rustBuilder.overrides.all ++ rust-overrides;
+          };
+        })
+      ];
+    };
+  in {
+    packages = {
+      many-rs = (pkgs.many-rs-pkgs.workspace.many {}).bin;
+      many-framework = (pkgs.many-framework-pkgs.workspace.many-ledger {}).bin;
+    };
+    devShells = {
+      many-rs = pkgs.many-rs-pkgs.workspaceShell {
+        LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+        nativeBuildInputs = [
+          pkgs.llvmPackages.libcxxClang
+        ];
+      };
+      many-framework = pkgs.many-framework-pkgs.workspaceShell {
+      };
+    };
+  });
+}
